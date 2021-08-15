2e35b22c9f016c5e89c475a2ca220d352ccafc40
Milton <A01634505@itesm.mx>
2021-08-15 17:17:35 +0000

    Finished test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) < 2 {
+		fmt.Println("Error: You did not provide any arguments")
+		return
+	}
+	name := ""
+
+	for i := 1; i < len(os.Args); i++ {
+		name += os.Args[i]
+		if i+1 < len((os.Args)) {
+			name += " "
+		}
+	}
+
+	fmt.Printf("Hello %v, Welcome to the jungle\n", name)
 }
