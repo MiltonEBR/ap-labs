f5092073eccd5255bd427328cd6aa7d3222ed5e0
Milton <A01634505@itesm.mx>
2021-09-26 17:37:58 +0000

    Finished str lib lab

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,35 @@
+//Milton Eduardo Barroso Ramírez.
+//A01634505
 #include <stdio.h>
 
-int main() {
+/*
+NOTE: I had to add prototype functions since mystradd() got assigned as a default return value int instead of char*,
+which resulted in a segementation fault, although the function does works
+*/
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+
+int main(int argc, char* argv[]) {
+
+    if(argc<4){
+        printf("Please use correct parameters <-find/-add> <String 1> <String 2>\n");
+        return 0;
+    }
+
+    if(mystrfind(argv[1], "-add")>-1){
+        char* res = mystradd(argv[2], argv[3]);
+        printf("Initial Length: %d\n",mystrlen(argv[2]));
+        printf("New String: %s\n",res);
+        printf("New Length: %d\n",mystrlen(res));
+
+    }else if(mystrfind(argv[1], "-find")>-1){
+        int f = mystrfind(argv[2], argv[3]);
+        printf("['%s'] string was found at [%d] position\n",argv[3],f);
+    }else{
+        printf("Please us the flag -add or -find\n");
+    }
+
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,78 @@
-
+//Milton Eduardo Barroso Ramírez.
+//A01634505
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    char *c = str;
+    int i = 0;
+
+    while(*c) {
+        i++;
+        c++;
+    };
+
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int oL = mystrlen(origin);
+    char* tmpO = origin;
+
+    int aL = mystrlen(addition);
+    char* tmpA = addition;
+
+    char* str = (char *) malloc(oL+aL);
+    char* res = str;
+
+    while(*tmpO){
+        *str = *tmpO;
+        str++;
+        tmpO++;
+    }
+    
+    while(*tmpA){
+        *str = *tmpA;
+        str++;
+        tmpA++;
+    }
+
+    return res;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+
+    char* o = origin;
+    char* s = substr;
+
+    int checking = -1;
+
+    int len = mystrlen(o);
+    int i = 0;
+
+    while(i<len){
+        if(checking>-1 && !*s) return checking;
+        else if (checking>-1){
+            if(*s == *o) {
+                s++;
+                o++;
+                i++;
+                continue;
+            }else{
+                checking = -1;
+                s = substr;
+            }
+        }
+
+        if(*s == *o){
+            checking = i;
+            s++;
+        }
+
+        o++;
+        i++;
+
+    }
+
+    return checking;
 }
