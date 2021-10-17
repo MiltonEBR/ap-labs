63423d57f4e5c1c7bbafa5355810c116a34ac0b2
Milton <A01634505@itesm.mx>
2021-10-17 22:16:20 +0000

    HashTable implemented

--- challenges/second-partial/c-dmesg-analyzer/dmesg-analyzer.c
+++ challenges/second-partial/c-dmesg-analyzer/dmesg-analyzer.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define REPORT_FILE "report.txt"
 
+#define HASHSIZE 101
+
+//Hash table implementation
+struct nlist{
+    struct nlist* next;
+    char* key;
+    char* val;
+};
+
+unsigned hash(char *s){
+    unsigned hashval;
+
+    for (hashval=0; *s != '\0'; s++) {
+        hashval = *s + 31 * hashval;
+    }
+
+    return hashval % HASHSIZE;
+}
+
+static struct nlist *hashtab[HASHSIZE];
+
+struct nlist* lookup(char *s){
+    struct nlist* np;
+
+    for (np=hashtab[hash(s)]; np!=NULL; np=np->next) {
+        if(strcmp(s, np->key)==0){
+            return np;
+        }
+    }
+    return NULL;
+}
+
+struct nlist* add(char* key, char* val){
+    struct nlist* np;
+    unsigned hashval;
+
+    if( (np=lookup(key) )==NULL){
+        np = (struct nlist* ) malloc(sizeof(*np));
+        if(np == NULL || (np->key = strdup(key))==NULL) return NULL;
+
+        hashval= hash(key);
+        np->next=hashtab[hash(key)];
+        hashtab[hashval]=np;
+    }else{
+        free((void*) np->val);
+    }
+    if((np->val=strdup(val))==NULL) return NULL;
+
+    return np;
+}
+
+
+
+//Analizer
 void analizeLog(char *logFile, char *report);
 
 int main(int argc, char **argv) {
@@ -10,6 +66,7 @@ int main(int argc, char **argv) {
 	printf("Usage:./dmesg-analizer logfile.txt\n");
 	return 1;
     }
+
 
     analizeLog(argv[1], REPORT_FILE);
 
